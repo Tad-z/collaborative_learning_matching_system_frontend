22ea84e2cf0f150219df627265b4dbaab7444b09
Tad-z <david.akintade1000@gmail.com>
2024-06-01 10:05:44 +0100

    progress

--- collab/src/components/Header.js
+++ collab/src/components/Header.js
@@ -2,9 +2,10 @@ import React from 'react'
 import './Header.css'
 import { Link } from 'react-router-dom';
 
-const Header = () => {
+const Header = ({ title }) => {
     return (
         <header>
+            <title>{title ? title + "-COLLAB" : "COLLAB"}</title>
             <div className="navbar">
                 <div>
                     <Link to="/" className='text-2xl font-bold'>CollabLearn</Link>
