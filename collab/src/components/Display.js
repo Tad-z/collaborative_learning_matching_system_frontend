b8ab3965c6c6e311ec53e1879e5e68b9010a3bdc
Tad-z <david.akintade1000@gmail.com>
2024-06-16 16:58:26 +0100

    display

--- collab/src/components/Display.js
+++ collab/src/components/Display.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { useLocation } from 'react-router-dom';
-import GroupList from './GroupList'; // Adjust the import path as needed
+import GroupList from './GroupList';
+import './GroupList.css';
 
 const Display = () => {
   const location = useLocation();
-  console.log(location.state)
+  console.log(location.state);
+  
   const { response } = location.state || {};
-
-  const { groups } = response
+  const groups = response ? response.groups : null;
 
   if (!groups) {
     return <p>No groups data available</p>;
@@ -16,7 +17,9 @@ const Display = () => {
   return (
     <div>
       <h1>Groups</h1>
-      <GroupList groups={groups} />
+      <div className='productContainer'>
+        <GroupList groups={groups} />
+      </div>
     </div>
   );
 };
--- collab/src/components/GroupList.js
+++ collab/src/components/GroupList.js
@@ -1,21 +1,22 @@
 import React from 'react';
+import './GroupList.css';
 
 const GroupList = ({ groups }) => {
   return (
     <div>
       {Object.keys(groups).map(groupName => (
-        <div key={groupName}>
+        <div key={groupName} className="card">
           <h3>{groupName}</h3>
           <ul>
             {groups[groupName].map(student => (
               <li key={student.student_id}>
-                <p>Student ID: {student.student_id}</p>
                 <p>Name: {student.first_name} {student.last_name}</p>
-                <p>Age: {student.age}</p>
+                {/* Uncomment the below lines if you want to display more details */}
+                {/* <p>Age: {student.age}</p>
                 <p>Gender: {student.gender}</p>
                 <p>Score: {student.score}</p>
                 <p>Cluster: {student.cluster}</p>
-                <p>Group: {student.group}</p>
+                <p>Group: {student.group}</p> */}
               </li>
             ))}
           </ul>
