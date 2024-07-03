c24cba14cb9da4d8064d37663b29ac4bd9743cab
Tad-z <david.akintade1000@gmail.com>
2024-07-03 14:59:16 +0100

    styles

--- collab/src/components/Header.js
+++ collab/src/components/Header.js
@@ -1,21 +1,25 @@
-import React from 'react'
-import './Header.css'
+import React from 'react';
 import { Link } from 'react-router-dom';
+import { Helmet } from 'react-helmet';
+import './Header.css';
+import logo from '../assets/collabee.png';
 
 const Header = ({ title }) => {
     return (
         <header>
-            <title>{title ? title + "-COLLAB" : "COLLAB"}</title>
+            <Helmet>
+                <title>{title ? `${title} - COLLAB` : 'COLLAB'}</title>
+            </Helmet>
             <div className="navbar">
                 <div>
-                    <Link to="/" className='text-2xl font-bold'>CollabLearn</Link>
+                    <img src={logo} alt="Collab logo" width={110} height={40} />
                 </div>
                 <div>
                     <Link to="/login" className='loginButton text-sm text-white'>Get Started</Link>
                 </div>
             </div>
         </header>
-    )
-}
+    );
+};
 
-export default Header
+export default Header;
